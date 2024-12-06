5202cff2947ab33a73fbb2ce1090207e36df59f6
IonesioJunior <ionesiojr@gmail.com>
2024-12-06 07:52:19 -0300

    Dataloader looks for the /private folder instead of /private/data_loader folder

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def gather_file_information(directory_path):
     """
     files_info = {}
 
+    files = [str(file) for file in Path(directory_path).rglob('*') if file.is_file()]
+    
     # Iterate over all items in the given directory
-    for item_name in os.listdir(directory_path):
-        item_path = os.path.join(directory_path, item_name)
-
+    for item_path in files:
         # Check if the item is a file
-        if os.path.isfile(item_path):
+        if os.path.isfile(item_path) and "syftperm" not in item_path:
             # Gather file information
             file_size = os.path.getsize(item_path)
             creation_time = os.path.getctime(item_path)
@@ -35,9 +35,11 @@ def gather_file_information(directory_path):
             )
 
             file_size = round(file_size / 1024, 2)
+            item_name = item_path.split("/")[-1]
             # Add the file info to the dictionary
             files_info[item_name] = {"size": file_size, "created": creation_date_str}
-
+    
+    print(files_info)
     return files_info
 
 
@@ -56,18 +58,18 @@ def save_to_json(data, output_path):
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 
-def get_app_private_data(client: Client, app_name: str) -> Path:
+def get_app_private_data(client: Client) -> Path:
     """
     Returns the private data directory of the app
     """
-    return client.workspace.data_dir / "private" / app_name
+    return client.workspace.data_dir / "private" 
 
 
 def main():
     client = Client.load()
 
     # Create the data loader private folder
-    dataset_private_folder = get_app_private_data(client, app_name="data_loader")
+    dataset_private_folder = get_app_private_data(client)
     os.makedirs(dataset_private_folder, exist_ok=True)
 
     data_loader = client.api_data("data_loader")
